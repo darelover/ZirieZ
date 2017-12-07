da347b77a3f3f5037c7ca7e9a0ee3149629ba9cf
darelover <email>
2017-12-07 10:47:40 +0530

    implemented swiping list items for edit and delete

--- app/src/main/java/com/example/atulsachdeva/ziriez/Adapters/ShowFirebaseRecyclerAdapter.java
+++ app/src/main/java/com/example/atulsachdeva/ziriez/Adapters/ShowFirebaseRecyclerAdapter.java
@@ -30,12 +30,7 @@ import java.util.HashMap;
  */
 
 public class ShowFirebaseRecyclerAdapter extends FirebaseRecyclerAdapter<Show, ShowFirebaseRecyclerAdapter.ShowViewHolder> {
-    /**
-     * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
-     * {@link FirebaseRecyclerOptions} for configuration options.
-     *
-     * @param options
-     */
+
     private Context context;
 
     public ShowFirebaseRecyclerAdapter(FirebaseRecyclerOptions<Show> options, Context context) {
@@ -54,185 +49,114 @@ public class ShowFirebaseRecyclerAdapter extends FirebaseRecyclerAdapter<Show, S
         return new ShowViewHolder(li.inflate(R.layout.list_item, parent, false));
     }
 
+    public void edit(int position) {
+
+        final String name = getItem(position).getName();
+        String episode = getItem(position).getEpisode();
+
+        final Dialog dialogEdit = new Dialog(context);
+        dialogEdit.setContentView(R.layout.dialog_edit);
+
+        ((EditText) dialogEdit.findViewById(R.id.etShowName))
+                .setText(name);
+        ((EditText) dialogEdit.findViewById(R.id.etShowEpisode))
+                .setText(episode);
+
+        ((Button) dialogEdit.findViewById(R.id.btnSave))
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        final Show updatedShow = new Show(
+                                ((EditText) dialogEdit.findViewById(R.id.etShowName))
+                                        .getText().toString(),
+                                ((EditText) dialogEdit.findViewById(R.id.etShowEpisode))
+                                        .getText().toString()
+                        );
+
+                        FirebaseDatabase
+                                .getInstance()
+                                .getReference("ShowList")
+                                .orderByChild("name")
+                                .equalTo(name)
+                                .addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(DataSnapshot dataSnapshot) {
+                                        String key = null;
+                                        for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
+                                            key = childSnapshot.getKey();
+                                        }
+                                        HashMap<String, Object> map = updatedShow.toMap();
+
+                                        HashMap<String, Object> childUpdates = new HashMap<>();
+                                        childUpdates.put(key, map);
+
+                                        FirebaseDatabase.getInstance().getReference("ShowList").updateChildren(childUpdates);
+                                    }
+
+                                    @Override
+                                    public void onCancelled(DatabaseError databaseError) {
+
+                                    }
+                                });
+                        notifyDataSetChanged();
+                        dialogEdit.dismiss();
+                    }
+                });
+        dialogEdit.findViewById(R.id.btnCancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogEdit.dismiss();
+            }
+        });
+        notifyDataSetChanged();
+        dialogEdit.show();
+    }
+
+    public void delete(int position) {
+        String name = getItem(position).getName();
+        FirebaseDatabase
+                .getInstance()
+                .getReference("ShowList")
+                .orderByChild("name")
+                .equalTo(name)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String key = null;
+                        for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
+                            key = childSnapshot.getKey();
+                        }
+//                            HashMap<String, Object> map = updatedShow.toMap();
+
+                        // removeValue()
+                        HashMap<String, Object> childUpdates = new HashMap<>();
+                        childUpdates.put(key, null);
+
+                        FirebaseDatabase.getInstance().getReference("ShowList").updateChildren(childUpdates);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+    }
+
     public class ShowViewHolder extends RecyclerView.ViewHolder {
 
-        LinearLayout llShowItem;
         TextView tvShowNname, tvShowEpisode;
-
-        Button btnPlus = new Button(context);
-        Button btnMinus = new Button(context);
-        Button btnEdit = new Button(context);
-        Button btnDelete = new Button(context);
 
         public ShowViewHolder(View itemView) {
             super(itemView);
 
-            llShowItem = itemView.findViewById(R.id.llShowItem);
             tvShowNname = itemView.findViewById(R.id.tvShowName);
             tvShowEpisode = itemView.findViewById(R.id.tvShowEpisode);
-
-            itemView.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View v) {
-
-                    final Dialog dialogOptionsListItem = new Dialog(context);
-                    dialogOptionsListItem.setContentView(R.layout.dialog_options_list_item);
-
-                    dialogOptionsListItem.findViewById(R.id.tvEdit)
-                            .setOnClickListener(new View.OnClickListener() {
-                                @Override
-                                public void onClick(View v) {
-
-                                    dialogOptionsListItem.dismiss();
-
-                                    final Dialog dialogEdit = new Dialog(context);
-                                    dialogEdit.setContentView(R.layout.dialog_edit);
-
-                                    ((EditText) dialogEdit.findViewById(R.id.etShowName))
-                                            .setText(tvShowNname.getText());
-                                    ((EditText) dialogEdit.findViewById(R.id.etShowEpisode))
-                                            .setText(tvShowEpisode.getText());
-
-                                    ((Button) dialogEdit.findViewById(R.id.btnSave))
-                                            .setOnClickListener(new View.OnClickListener() {
-                                                @Override
-                                                public void onClick(View v) {
-                                                    edit(tvShowNname.getText().toString(),
-                                                            new Show(
-                                                            ((EditText) dialogEdit.findViewById(R.id.etShowName))
-                                                                    .getText().toString(),
-                                                            ((EditText) dialogEdit.findViewById(R.id.etShowEpisode))
-                                                                    .getText().toString()
-                                                    ));
-                                                    notifyDataSetChanged();
-                                                    dialogEdit.dismiss();
-                                                }
-                                            });
-                                    dialogEdit.findViewById(R.id.btnCancel).setOnClickListener(new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View v) {
-                                            dialogEdit.dismiss();
-                                        }
-                                    });
-                                    dialogEdit.show();
-
-                                }
-                            });
-
-                    dialogOptionsListItem.findViewById(R.id.tvDelete)
-                            .setOnClickListener(new View.OnClickListener() {
-                                @Override
-                                public void onClick(View v) {
-
-                                    dialogOptionsListItem.dismiss();
-
-                                    delete(tvShowNname.getText().toString());
-                                }
-                            });
-
-                    dialogOptionsListItem.show();
-
-
-                    /*
-                    Log.d("HI", "onLongClick: " + pxToDp(tvShowNname.getWidth()));
-                    tvShowNname.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-//                    tvShowEpisode.setLayoutParams(new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT));
-
-                    btnPlus.setLayoutParams(new LinearLayout.LayoutParams(dpToPx(50), ViewGroup.LayoutParams.WRAP_CONTENT));
-                    btnMinus.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-                    btnEdit.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-                    btnDelete.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-
-                    btnPlus.setText("+");
-                    btnMinus.setText("-");
-                    btnEdit.setText("EDIT");
-                    btnDelete.setText("DELETE");
-
-                    llShowItem.addView(btnPlus);
-                    llShowItem.addView(btnMinus);
-                    llShowItem.addView(btnEdit);
-                    llShowItem.addView(btnDelete);
-
-                    Log.d("HI", "onLongClick: " + pxToDp(tvShowNname.getWidth()));
-//                    Log.d("HI", "onLongClick: ");
-                    */
-
-                    return false;
-                }
-            });
         }
 
         void bindView(Show thisShow) {
             tvShowNname.setText(thisShow.getName());
             tvShowEpisode.setText(thisShow.getEpisode());
         }
-
-        public int pxToDp(int px) {
-            DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
-            int dp = Math.round(px / (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
-            return dp;
-        }
-
-        public int dpToPx(int dp) {
-            DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
-            int px = Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
-            return px;
-        }
-
-        void edit(String oldName, final Show updatedShow) {
-            FirebaseDatabase
-                    .getInstance()
-                    .getReference("ShowList")
-                    .orderByChild("name")
-                    .equalTo(oldName)
-                    .addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(DataSnapshot dataSnapshot) {
-                            String key = null;
-                            for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
-                                key = childSnapshot.getKey();
-                            }
-                            HashMap<String, Object> map = updatedShow.toMap();
-
-                            HashMap<String, Object> childUpdates = new HashMap<>();
-                            childUpdates.put(key, map);
-
-                            FirebaseDatabase.getInstance().getReference("ShowList").updateChildren(childUpdates);
-                        }
-
-                        @Override
-                        public void onCancelled(DatabaseError databaseError) {
-
-                        }
-                    });
-        }
-        void delete(String name) {
-            FirebaseDatabase
-                    .getInstance()
-                    .getReference("ShowList")
-                    .orderByChild("name")
-                    .equalTo(name)
-                    .addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(DataSnapshot dataSnapshot) {
-                            String key = null;
-                            for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
-                                key = childSnapshot.getKey();
-                            }
-//                            HashMap<String, Object> map = updatedShow.toMap();
-
-                            HashMap<String, Object> childUpdates = new HashMap<>();
-                            childUpdates.put(key, null);
-
-                            FirebaseDatabase.getInstance().getReference("ShowList").updateChildren(childUpdates);
-                        }
-
-                        @Override
-                        public void onCancelled(DatabaseError databaseError) {
-
-                        }
-                    });
-        }
-
     }
 }
--- app/src/main/java/com/example/atulsachdeva/ziriez/MainActivity.java
+++ app/src/main/java/com/example/atulsachdeva/ziriez/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.atulsachdeva.ziriez;
 
 import android.app.Dialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.drawable.Icon;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.atulsachdeva.ziriez.Adapters.ShowFirebaseRecyclerAdapter;
 import com.example.atulsachdeva.ziriez.Models.Show;
@@ -38,7 +46,9 @@ public class MainActivity extends AppCompatActivity {
 //        FirebaseDatabase.getInstance().setPersistenceEnabled(true);   // what changes to be implemented in project when using this ?
         final DatabaseReference reference = FirebaseDatabase.getInstance().getReference("ShowList");
 
-        Query showListQuery = reference.limitToFirst(10);
+//        for pulling all the data and limit it to 20 key- value pairs, following line is replaced by line following it
+//        Query showListQuery = reference.limitToFirst(20);
+        Query showListQuery = reference;
 
         FirebaseRecyclerOptions<Show> options =
                 new FirebaseRecyclerOptions.Builder<Show>()
@@ -47,6 +57,92 @@ public class MainActivity extends AppCompatActivity {
 
         adapter = new ShowFirebaseRecyclerAdapter(options, this);
         rvList.setAdapter(adapter);
+
+        // for implementing swipe events
+        ItemTouchHelper.SimpleCallback helper = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+
+            public int convertDpToPx(int dp) {
+                return Math.round(dp * (getResources().getDisplayMetrics().xdpi / DisplayMetrics.DENSITY_DEFAULT));
+            }
+
+            public float ALPHA_FULL = (float) 1.0;
+
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+
+                int position = viewHolder.getAdapterPosition();
+
+                // delete
+                if (direction == ItemTouchHelper.LEFT) {
+                    adapter.delete(position);
+                }
+                // edit
+                else if (direction == ItemTouchHelper.RIGHT) {
+                    adapter.edit(position);
+                }
+            }
+
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+
+                if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+
+                    View itemView = viewHolder.itemView;
+
+                    Paint paint = new Paint();
+                    Bitmap icon;
+
+                    // right swipe -- edit
+                    if (dX > 0) {
+                        paint.setARGB(255, 0, 255, 0);
+                        c.drawRect(
+                                (float) itemView.getLeft(),
+                                (float) itemView.getTop(),
+                                dX,
+                                (float) itemView.getBottom(),
+                                paint
+                        );
+                        icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_edit_white_24dp);
+                        c.drawBitmap(
+                                icon,
+                                (float) itemView.getLeft() + convertDpToPx(16),
+                                (float) itemView.getTop() + ((float) itemView.getBottom() - (float) itemView.getTop() - icon.getHeight()) / 2,
+                                paint
+                        );
+                    }
+                    // left swipe -- delete
+                    else {
+                        paint.setARGB(255, 255, 0, 0);
+                        c.drawRect(
+                                (float) itemView.getRight() + dX,
+                                (float) itemView.getTop(),
+                                (float) itemView.getRight(),
+                                (float) itemView.getBottom(),
+                                paint
+                        );
+                        icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_delete_white_24dp);
+                        c.drawBitmap(
+                                icon,
+                                (float) itemView.getRight() - convertDpToPx(16) - icon.getWidth(),
+                                (float) itemView.getTop() + ((float) itemView.getBottom() - (float) itemView.getTop() - icon.getHeight()) / 2,
+                                paint
+                        );
+                    }
+
+                    // Fade out the view when it is swiped out of the parent
+                    final float alpha = ALPHA_FULL - Math.abs(dX) / (float) viewHolder.itemView.getWidth();
+                    viewHolder.itemView.setAlpha(alpha);
+                    viewHolder.itemView.setTranslationX(dX);
+                }
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+        };
+        new ItemTouchHelper(helper).attachToRecyclerView(rvList);
 
         fabAdd.setOnClickListener(new View.OnClickListener() {
             @Override
